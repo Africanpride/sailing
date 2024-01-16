34790ffa4e5c32e4e4e1f967567c56520c7d8d74
Pius Opoku-Fofie <pius1919@gmail.com>
2024-01-16 11:12:16 +0000

    starting...

---
+++ app/Models/User.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens;
+    use HasFactory;
+    use HasProfilePhoto;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
+    use HasUlids;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'firstName',
+        'lastName',
+        'email',
+        'email_verified_at',
+        'password',
+        'avatar',
+        'social_avatar',
+        'facultyMember',
+        'participant',
+        'must_create_password',
+        'staff',
+        'active',
+        'ban',
+        'google_id',
+        'apple_id'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'facultyMember' => 'boolean',
+        'participant' => 'boolean',
+        'must_create_password' => 'boolean',
+        'staff' => 'boolean',
+        'active' => 'boolean'
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array<int, string>
+     */
+    protected $appends = [
+        'profile_photo_url',
+    ];
+}
---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            // $table->id();
+            $table->ulid('id')->primary();
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('avatar')->nullable();
+            $table->string('social_avatar')->nullable();
+            $table->boolean('facultyMember')->default(false);
+            $table->boolean('participant')->default(true);
+            $table->boolean('must_create_password')->default(false);
+            $table->boolean('staff')->default(false);
+            $table->boolean('active')->default(false);
+            $table->boolean('ban')->default(false);
+            $table->string('google_id')->nullable();
+            $table->string('apple_id')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
