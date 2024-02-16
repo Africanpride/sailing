852ded39fb65098b541bde9d2d869e04ecbed80f
Pius Opoku-Fofie <pius1919@gmail.com>
2024-02-16 13:37:53 +0000

    cookie issue resolved

--- resources/views/components/cookie.blade.php
+++ resources/views/components/cookie.blade.php
@@ -1,4 +1,5 @@
 <div class="transitiondelay-500  duration-700 ease-in-out" id="hs-modal-upgrade-to-pro">
+    <!-- Cookie Consent by TermsFeed https://www.TermsFeed.com -->
     <script type="text/javascript" src="https://www.termsfeed.com/public/cookie-consent/4.1.0/cookie-consent.js"
         charset="UTF-8"></script>
     <script type="text/javascript" charset="UTF-8">
@@ -6,7 +7,7 @@
             cookieconsent.run({
                 "notice_banner_type": "simple",
                 "consent_type": "express",
-                "palette": "white",
+                "palette": "dark",
                 "language": "en",
                 "page_load_consent_levels": ["strictly-necessary"],
                 "notice_banner_reject_button_hide": false,
@@ -18,10 +19,12 @@
         });
     </script>
 
-    <noscript>
-        Cookie consent management tool by
-        <a href="https://www.termsfeed.com/privacy-policy-generator/">
-            TermsFeed Free Privacy Policy    Generator
-        </a>
-    </noscript>
+    <noscript>Free cookie consent management tool by <a
+            href="https://www.termsfeed.com/privacy-policy-generator/">TermsFeed Free Privacy Policy
+            Generator</a></noscript>
+    <!-- End Cookie Consent by TermsFeed https://www.TermsFeed.com -->
+
+
+    <!-- Below is the link that users can use to open Preferences Center to change their preferences. Do not modify the ID parameter. Place it where appropriate, style it as needed. -->
+
 </div>
