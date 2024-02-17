64f16d9415971adb9aac98296e48f1d1529b003d
Pius Opoku-Fofie <pius1919@gmail.com>
2024-02-17 11:24:21 +0000

    mobile-menu w-full

--- resources/views/components/all-modals.blade.php
+++ resources/views/components/all-modals.blade.php
@@ -332,7 +332,7 @@
 
                         <a href="{{ route('institutes.index') }}">
                             <button type="button"
-                                class="py-3 px-4 flex items-center justify-between gap-x-2 text-sm font-medium cursor-pointer rounded-lg border w-full border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 uppercase ">
+                                class="py-3 px-4 flex items-center justify-between gap-x-2 text-xs font-medium cursor-pointer rounded-lg border w-full border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 uppercase ">
                                 <span>Institutes</span>
                                 <x-lucide-globe-2 class="w-5 h-5 text-current" />
                             </button>
--- resources/views/components/mobile-menu.blade.php
+++ resources/views/components/mobile-menu.blade.php
@@ -1,7 +1,8 @@
 
   <a href="{{ route($route , $slug ?? '') }}">
-    <button type="button" class="py-3 px-4 flex items-center justify-between gap-x-2 text-sm font-medium rounded-lg border w-full border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 uppercase ">
+    <button type="button" class="py-3 px-4 flex items-center justify-between gap-x-2 text-xs font-medium rounded-lg border w-full border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 uppercase  ">
         {{ $slot }}
-      <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg>
+        <x-lucide-arrow-right class="w-5 h-5 text-current" />
+
     </button>
   </a>
