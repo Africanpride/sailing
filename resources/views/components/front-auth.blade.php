fd080c729b9dbae85d17cf6c6e4fc2507fb511ad
Pius Opoku-Fofie <pius1919@gmail.com>
2024-02-17 11:20:17 +0000

    mobile-menu w-full

--- resources/views/components/front-auth.blade.php
+++ resources/views/components/front-auth.blade.php
@@ -96,7 +96,7 @@
                 <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
                 <circle cx="12" cy="7" r="4" />
             </svg>
-            <span> Log In/Signup</span>
+            <span class="text-xs"> Log In/Signup</span>
         </a>
     @endguest
 </div>
