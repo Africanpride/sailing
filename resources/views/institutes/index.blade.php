342d8576a2f0ea73ecec34c9212ecc22a4dba9f3
Pius Opoku-Fofie <pius1919@gmail.com>
2024-02-21 14:19:11 +0000

    whew

--- resources/views/institutes/index.blade.php
+++ resources/views/institutes/index.blade.php
@@ -85,7 +85,7 @@
                     <div class="w-full h-auto ">
                         <img src="{{ $institute->featured_image }}" alt=""
                             class="rounded-t-2xl object-cover  shadow-2xl h-48 w-full ">
-                        <div class="bg-white shadow-2xl rounded-b-2xl pb-2 ">
+                        <div class="bg-white border border-s-0 rounded-b-2xl pb-2 ">
 
 
                             <div class="w-5/6 m-auto h-[7rem] min-h-[7rem] space-y-3">
@@ -105,13 +105,7 @@
                                 </a>
                             </div>
 
-                            {{-- <div class="text-center m-auto mt-6 w-full h-16">
-                                <a href="{{ route('institutes.show', [$institute]) }}"
-                                    class="text-gray-500 font-bold lg:text-sm hover:text-gray-900
-                                uppercase ">More About
-                                    {{ $institute->editions()->latest()->first()->acronym }}</a>
-                            </div> --}}
-                            <div class="bg-indigo-100/50 grid grid-cols-4 m-auto md:p-3 mx-2 p-3 rounded-2xl w-auto">
+                            <div class="bg-firefly-100/50 grid grid-cols-4 m-auto md:p-3 mx-2 p-3 rounded-2xl w-auto">
                                 <div class="col-span-1 ">
                                     <img class="w-15 md:w-15 md:h-15" src="{{ $institute->institute_logo }}"
                                         alt="{{ $institute->acronym }}">
