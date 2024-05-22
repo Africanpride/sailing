dfc13e86f734b8865106cea008bd6157ce12c5c6
Pius Opoku-Fofie <pius1919@gmail.com>
2024-05-22 08:50:59 +0000

    updatestaff updated...

--- app/Livewire/UpdateStaff.php
+++ app/Livewire/UpdateStaff.php
@@ -29,15 +29,14 @@ class UpdateStaff extends ModalComponent
     {
         return false;
     }
+
     public static function closeModalOnClickAway(): bool
     {
         return false;
     }
 
     protected static array $maxWidths = [
-
         '5xl' => 'max-w-2xl md:max-w-xl lg:max-w-3xl xl:max-w-5xl',
-
     ];
 
     public function mount(User $user)
@@ -47,10 +46,10 @@ class UpdateStaff extends ModalComponent
         $this->lastName     = $user->lastName;
         $this->email        = $user->email;
         $this->avatar       = $user->avatar_url;
-        // $this->staffRoles   = $user->roles->pluck('id')->toArray();
+        // Convert role IDs to strings to ensure compatibility with ULID and UUID
         $this->staffRoles = $user->roles->pluck('id')->map(fn($roleId) => (string) $roleId)->toArray();
         $this->active       = $user->active;
-        $this->roles = Role::all(); // we shall check against this
+        $this->roles = Role::all(); // We shall check against this
     }
 
     public function updateStaff()
@@ -61,10 +60,11 @@ class UpdateStaff extends ModalComponent
             'email' => 'required|unique:users,email,' . $this->user->id,
             'active' => 'required|boolean',
         ]);
-        // update user with validated details
+
+        // Update user with validated details
         $this->user->update($data);
 
-        // activate staff account
+        // Activate staff account
         if ($this->active) {
             $this->user->makeVisible('active')->toArray();
             $this->user->forceFill([
@@ -72,25 +72,22 @@ class UpdateStaff extends ModalComponent
             ])->save();
         }
 
-        // update user roles
-        // because of ULID and UUID, the role id's need to be converted to integers else it would
-        // see it as string
-
+        // Update user roles
+        // Because of ULID and UUID, the role IDs need to be converted to integers
         $updatedRoles = collect($this->staffRoles)->map(fn ($roleId) => (int) $roleId)->toArray();
+        $this->user->syncRoles($updatedRoles);
 
-        $this->user->syncRoles([...$updatedRoles]);
-
-        // send reset password
+        // Send reset password
         if ($this->resetPassword) {
             $this->sendPasswordResetLink($this->user->email);
         }
 
-        // alert/Toast to show success
+        // Alert/Toast to show success
         app('flasher')->addSuccess('success', 'Staff Account Updated!');
-        // return to staff page
+
+        // Return to staff page
         return redirect()->to('staff');
     }
-
 
     public function render()
     {
